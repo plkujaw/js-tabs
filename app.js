bd456ccd856f100aecc8270a796a158d802ea824
plkujaw <jawor.jakub@gmail.com>
2020-07-06 14:00:08 +0100

    project done

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const about = document.querySelector(".about");
 const articles = document.querySelectorAll(".content");
 
 about.addEventListener("click", (event) => {
-  // target buttons by checking if clicked target has a dataset id
+  // target buttons by checking if clicked target has a dataset=id
   const id = event.target.dataset.id;
   if (id) {
+    // remove active form buttons and add only to the clicked one
     buttons.forEach((button) => {
       button.classList.remove("active");
       event.target.classList.add("active");
     });
+    // hide articles and display only one with matching clicked button id
+    articles.forEach((article) => {
+      article.classList.remove("active");
+    });
+    const tabContent = document.getElementById(id);
+    tabContent.classList.add("active");
   }
 });
